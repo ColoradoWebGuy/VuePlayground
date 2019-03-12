9526c8a3c329f8df2b760480afdae6e61428e3c6
ColoradoWebGuy <sschulte@secc.org>
2019-03-12 15:02:46 -0500

    Intro to Vue - Event Handling

--- vue_mastery/main.js
+++ vue_mastery/main.js
@@ -2,20 +2,37 @@
 var app = new Vue({
      el: '#app',
      data: {
-         product: "Socks",
-         image: "./assets/vmSocks-green.jpg",
-         inStock: true,
-         details: ["80% cotton", "20% polyester", "Gender-neutral"],
-         sizes: ['x-small','small','medium','large','x-large'],
-         variants: [
-            {
+        product: 'Socks',
+        image: './assets/vmSocks-green.jpg',
+        inStock: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+          {
             variantId: 2234,
-            variantColor: 'green'
-            },
-            {
+            variantColor: 'green',
+            variantImage: './assets/vmSocks-green.jpg'
+          },
+          {
             variantId: 2235,
-            variantColor: 'blue'
-            }
-        ]
+            variantColor: 'blue',
+            variantImage: './assets/vmSocks-blue.jpg'
+          }
+        ],
+        cart: 0
+     },
+     methods: {
+         addToCart() {
+             this.cart += 1
+         },
+         subtractFromCart() {
+             this.cart -= 1
+         },
+         // Instead of writing out an anonymous function like
+         // updateProduct: function(variantImage), we can use the ES6
+         // shorthand and just say updateProduct(variantImage).
+         // These are equivalent ways of saying the same thing.
+         updateProduct(variantImage) {
+             this.image = variantImage
+         }
      }
  })
