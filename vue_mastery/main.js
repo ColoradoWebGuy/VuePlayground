070bbb7613d08386be0e156b87f06cb698af142e
ColoradoWebGuy <sschulte@secc.org>
2019-03-12 11:58:14 -0500

    Intro to Vue - The Vue Instance

--- vue_mastery/main.js
+++ vue_mastery/main.js
@@ -1,8 +1,12 @@
- // var product = "Socks";
 
+// this object has a variety of optional properties that give the instance
+// the ability to store data and perform actions.
  var app = new Vue({
+     // forming a relationship between the instance, 'app' and that
+     // portion of the DOM 'div#app'
      el: '#app',
      data: {
-         message: "Hello World"
+         product: "Socks",
+         description: "A pair of warm, fuzzy socks"
      }
  })
