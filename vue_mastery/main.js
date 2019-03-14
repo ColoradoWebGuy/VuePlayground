e76b0393aa8589c01e3a81fc323333ad81833def
ColoradoWebGuy <sschulte@secc.org>
2019-03-14 13:34:36 -0400

    Intro to Vue - Computed Properties

--- vue_mastery/main.js
+++ vue_mastery/main.js
@@ -3,29 +3,46 @@ var app = new Vue({
      el: '#app',
      data: {
         product: 'Socks',
-        image: './assets/vmSocks-green.jpg',
-        inStock: false,
+        brand: 'Vue Mastery',
+        selectedVariant: 0,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
           {
             variantId: 2234,
             variantColor: 'green',
-            variantImage: './assets/vmSocks-green.jpg'
+            variantImage: './assets/vmSocks-green.jpg',
+            variantQuantity: 10,
           },
           {
             variantId: 2235,
             variantColor: 'blue',
-            variantImage: './assets/vmSocks-blue.jpg'
+            variantImage: './assets/vmSocks-blue.jpg',
+            variantQuantity: 0,
           }
         ],
-        cart: 0
+        cart: 0,
+        onSale: true
      },
      methods: {
          addToCart() {
              this.cart += 1
          },
-         updateProduct(variantImage) {
-             this.image = variantImage
+         updateProduct(index) {
+             this.selectedVariant = index
+         }
+     },
+     computed: {
+         title() {
+             return this.brand + ' ' + this.product
+        },
+         image() {
+             return this.variants[this.selectedVariant].variantImage
+         },
+         inStock() {
+             return this.variants[this.selectedVariant].variantQuantity
+         },
+         sale() {
+             return (this.onSale) ? this.title + ' are on sale!' : this.title + ' are not on sale'
          }
      }
  })
